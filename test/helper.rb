811bb1783a2e3671e45bc7ad9890e0edf3b34018
Ryan Kuykendall <ryank@drizzle.com>
2010-01-01 21:44:53 -0800

    setting up gem files

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'model_citizen'
+
+class ModelCitizenTest < Test::Unit::TestCase
+end
